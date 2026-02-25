faa906b10683e1fe23dd5f84977ea3c58afb25b2
Waseem Habib <w@W-339.lan>
2026-02-24 16:40:40 -0800

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./ThemeContext";
+import { inject } from "@vercel/analytics";
 import App from "./App";
+
+inject();
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
